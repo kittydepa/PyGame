e309736ff288acd0f1b6f4d210c86842cad0c902
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-10-17 12:31:28 +0200

    Added comments

--- practice/projects/skeleton/bin/first_example.py
+++ practice/projects/skeleton/bin/first_example.py
@@ -10,8 +10,8 @@ pygame.init() # Need to call this every time you use pygame, else you will get a
 DISPLAYSURF = pygame.display.set_mode((400, 300)) # This returns as a pygame.surface object - will be explained later on in the book. Also, need to set these dimensions as a tuple, not just plain intergers. That's why there are two sets of ()
 pygame.display.set_caption("Kitty's Super Cute Game")
 while True: #main game loop
-    for event in pygame.event.get():
-        if event.type == QUIT:
+    for event in pygame.event.get(): # To iterate over the list of Event objects that was returned by pygame.event.get()
+        if event.type == QUIT: # This checks to see if the event object is equal to QUIT
             pygame.quit()
             sys.exit()
     pygame.display.update()
