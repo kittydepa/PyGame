b06ed3ee096b25ab08b9f253b5f5848d9352e6c0
Kitty Depa <51916561+kittydepa@users.noreply.github.com>
2021-10-17 12:14:24 +0200

    Added comments

--- practice/projects/skeleton/bin/first_example.py
+++ practice/projects/skeleton/bin/first_example.py
@@ -6,8 +6,8 @@ CLI vs GUI in PyGame
 import pygame, sys
 from pygame.locals import *
 
-pygame.init()
-DISPLAYSURF = pygame.display.set_mode((400, 300))
+pygame.init() # Need to call this every time you use pygame, else you will get an error 
+DISPLAYSURF = pygame.display.set_mode((400, 300)) # This returns as a pygame.surface object - will be explained later on in the book. Also, need to set these dimensions as a tuple, not just plain intergers. That's why there are two sets of ()
 pygame.display.set_caption("Kitty's Super Cute Game")
 while True: #main game loop
     for event in pygame.event.get():
