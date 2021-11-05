5d1300aee9a36e34624b61cf3371a87afb85e3c1
Kitty Depa <katherinedepa@gmail.com>
2021-11-05 16:12:53 +0100

    quick update

--- practice/projects/skeleton/bin/first_example.py
+++ practice/projects/skeleton/bin/first_example.py
@@ -7,7 +7,7 @@ import pygame, sys
 from pygame.locals import *
 
 pygame.init() # Need to call this every time you use pygame, else you will get an error 
-DISPLAYSURF = pygame.display.set_mode((400, 300)) # This returns as a pygame.surface object - will be explained later on in the book. Also, need to set these dimensions as a tuple, not just plain intergers. That's why there are two sets of ()
+DISPLAYSURF = pygame.display.set_mode((480, 320)) # This returns as a pygame.surface object - will be explained later on in the book. Also, need to set these dimensions as a tuple, not just plain intergers. That's why there are two sets of ()
 pygame.display.set_caption("Kitty's Super Cute Game")
 while True: #main game loop
     for event in pygame.event.get(): # To iterate over the list of Event objects that was returned by pygame.event.get()
